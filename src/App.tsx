7d905f612f7651787df4439ab805e39a83fb5f30
Piotr Matlak <piotr.matlak.contact@gmail.com>
2023-10-03 10:06:49 +0200

    create base layout and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,57 @@
-function App() {
+import Footer from "components/footer";
+import Menu from "components/menu";
+import Navbar from "components/navbar";
+import Home from "pages/home";
+import Login from "pages/login";
+import Products from "pages/products";
+import Users from "pages/users";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import "styles/global.scss";
 
-  return (
-    <div>Hello World</div>
-  )
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/components/menu/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Menu = () => {
+  return <div className="menu">menu</div>;
+};
+
+export default Menu;
