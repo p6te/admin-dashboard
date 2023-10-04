8556d0bc6ac76a0ee4284cddf780c15036e5a205
Piotr Matlak <piotr.matlak.contact@gmail.com>
2023-10-04 09:29:30 +0200

    add navbar styling

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import "./navbar.scss";
 
 const Navbar = () => {
-  return <div className="navbar">navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.png" alt="logo" />
+      </div>
+      <div className="icons">
+        <img src="search.svg" alt="" className="icon" />
+        <img src="app.svg" alt="" className="icon" />
+        <img src="expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="avatar.jpg" alt="avatar" />
+          <span>Tom</span>
+        </div>
+        <img src="settings.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
