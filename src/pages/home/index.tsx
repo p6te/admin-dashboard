8c646f68cae7181d87b136a2969d2edc8e2b0275
Piotr Matlak <piotr.matlak.contact@gmail.com>
2023-10-06 10:55:55 +0200

    add top deals component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,22 @@
+import TopBox from "components/topBox";
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="box box1">
+        <TopBox />
+      </div>
+      <div className="box box2">2</div>
+      <div className="box box3">3</div>
+      <div className="box box4">4</div>
+      <div className="box box5">5</div>
+      <div className="box box6">6</div>
+      <div className="box box7">7</div>
+      <div className="box box8">8</div>
+      <div className="box box9">9</div>
+    </div>
+  );
 };
 
 export default Home;
